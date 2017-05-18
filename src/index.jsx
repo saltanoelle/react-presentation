c911655cda23c71230c5826b202fda7d29648c6a
Vladimir Mikhaylovskiy <vlad@indiegogo.com>
2017-05-17 23:42:29 -0700

    removed components

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Route, HashRouter } from "react-router-dom";
-import Home from "./home";
-import RepoList from "./repo_list";
 
 function Header() {
   return (
@@ -16,8 +14,6 @@ ReactDOM.render((
   <HashRouter>
     <div className="container">
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/repos" component={RepoList} />
     </div>
   </HashRouter>
 ), document.getElementById("container"));
